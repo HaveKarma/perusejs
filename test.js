d8bdcb0c36342d2bcef98c7c7f92f168f4439227
Justin Reynard <framerate@gmail.com>
2014-02-02 16:03:13 -0800

    jshint fix.

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ var peruse = require('./peruse');
 
 console.log('Setting up Scraper...');
 var scraper = new peruse(
-        {
-            'selector': 'p.message',
-            'baseUrl': 'https://www.airbnb.com/users/show/',
-            'identifier': '492445'
-        }
+    {
+        'selector': 'p.message',
+        'baseUrl': 'https://www.airbnb.com/users/show/',
+        'identifier': '492445'
+    }
     );
 
 console.log('calling process()');
